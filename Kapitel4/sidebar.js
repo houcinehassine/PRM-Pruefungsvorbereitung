84f5628921f4e5fb30da5f05f7b788f035d5925e
houcinehassine <houcine.hassine@gmail.de>
2026-05-29 10:06:10 +0200

    weiter mit K4

--- Kapitel4/sidebar.js
+++ Kapitel4/sidebar.js
@@ -8,7 +8,7 @@ class SiteSidebar extends HTMLElement {
           <ul>
 
             <!-- Grundlagen -->
-            <li class="toc-page"><a href="Page1.html">Grundlagen</a></li>
+            <li class="toc-page"><a href="Page1.html">1. Grundlagen</a></li>
             <li class="toc-sub"><a href="Page1.html#ziel">Ziel und Einordnung</a></li>
             <li class="toc-sub"><a href="Page1.html#typen">Grundtypen</a></li>
             <li class="toc-sub"><a href="Page1.html#kmeans">k-means</a></li>
@@ -17,14 +17,14 @@ class SiteSidebar extends HTMLElement {
             <li class="toc-sub"><a href="Page1.html#ablauf">Formaler Ablauf</a></li>
 
             <!-- k-means vertieft -->
-            <li class="toc-page"><a href="Page2.html">Algorithmus & Grenzen</a></li>
+            <li class="toc-page"><a href="Page2.html">2. k-means und Varianten</a></li>
             <li class="toc-sub"><a href="Page2.html#scratch">k-means from Scratch</a></li>
             <li class="toc-sub"><a href="Page2.html#schwaechen">Schwächen</a></li>
             <li class="toc-sub"><a href="Page2.html#varianten">Varianten</a></li>
             <li class="toc-sub"><a href="Page2.html#vor-nachteile">Vor- und Nachteile</a></li>
 
             <!-- Validierung -->
-            <li class="toc-page"><a href="Page3.html">Gütekriterien & Auswahl von k</a></li>
+            <li class="toc-page"><a href="Page3.html">3. Gütekriterien & Auswahl von k</a></li>
             <li class="toc-sub"><a href="Page3.html#arten">Validierung</a></li>
             <li class="toc-sub"><a href="Page3.html#masse">Validierungsmaße</a></li>
             <li class="toc-sub"><a href="Page3.html#elbow">SSE und Elbow Plot</a></li>
@@ -32,7 +32,7 @@ class SiteSidebar extends HTMLElement {
             <li class="toc-sub"><a href="Page3.html#scaling">Scaling</a></li>
 
             <!-- DBSCAN -->
-            <li class="toc-page"><a href="Page4.html">Dichtebasiertes Clustering</a></li>
+            <li class="toc-page"><a href="Page4.html">4. dichtebasiertes Clustering</a></li>
             <li class="toc-sub"><a href="Page4.html#dbscan">DBSCAN</a></li>
             <li class="toc-sub"><a href="Page4.html#knn">Neue Punkte zuordnen</a></li>
             <li class="toc-sub"><a href="Page4.html#brownfield">Brownfield-Beispiel</a></li>
@@ -43,7 +43,7 @@ class SiteSidebar extends HTMLElement {
             <li class="toc-sub"><a href="Page5.html#code">Code-Sammlung</a></li>
             <li class="toc-sub"><a href="Page5.html#stichwort">Stichwortverzeichnis</a></li>
 
-            <li class="toc-page"><a href="Page6.html" class="${active === 'page6' ? 'active' : ''}">Reflexion & Vertiefung</a></li>
+            <li class="toc-page"><a href="Page6.html" ${active === 'page6' ? 'class="active"' : ''}>Reflexion & Vertiefung</a></li>
             <li class="toc-sub"><a href="Page6.html#reflexion">Reflexionsfragen</a></li>
             <li class="toc-sub"><a href="Page6.html#dbscan-hyper">DBSCAN Hyperparameter</a></li>
             <li class="toc-sub"><a href="Page6.html#dbscan-neue-punkte">Neue Punkte zuordnen</a></li>
